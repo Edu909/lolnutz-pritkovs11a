5c9524fadd258e5ba424970e9ddb5a5d2cd1cc69
Edu909 <edikpritkov@gmail.com>
2024-04-02 14:17:48 +0300

    e

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ def index():
 def te():
     return render_template("teksts.html")
 
+
+@app.route("/saraksts")
+def saraksts():
+    saraksts = ["big", "Black", "Balls"]
+    bildes = ["https://play-lh.googleusercontent.com/jnutN_x8MZeuK_N5qrq6KjUa8ecuaFisDY49VCB3tV4OrcqN0euqzIlGYIzsITo5dcZS", "https://blackdesign.world/app/uploads/2020/07/BLACK-icon.jpg", "https://d5wt70d4gnm1t.cloudfront.net/media/a-s/artworks/gardar-eide-einarsson/44155-176986968416/gardar-eide-einarsson-black-balls-800x800.jpg"]
+    return render_template("saraksts.html", vardi = saraksts)
+
+
 if __name__ == '__main__':
     app.run(port = 5000)
     
